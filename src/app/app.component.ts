e36c30d6f5f39336c9a28a9a742b312a9cfd2bc3
alexey-masterkov <mastrukov@yahoo.ca>
2020-10-13 20:43:18 -0400

    kk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'demo';
+  title: string = 'Angular Demo';
+  leadParagraph: string = 'All new componets in paragraph';
+
+  order(){
+    console.log("Biuu");
+  }
 }
